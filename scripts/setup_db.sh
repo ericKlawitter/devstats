bb96223ce4ccde4106e84f82758b310beacd0de6
Eric Klawitter <5823687+ericKlawitter@users.noreply.github.com>
2019-04-18 22:42:25 -0700

    Manually add gha_logs table

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -2,4 +2,5 @@
 
 PGUSER=postgres PGDATABASE=postgres PG_PASS=${PG_PASS} PG_PASS_RO=${PG_PASS} PG_PASS_TEAM=knative ./devel/init_database.sh
 ./structure
-
+#TODO investigate why gha_logs gets created in the wrong database
+pg_dump -t gha_logs knative | psql devstats
